1fa1a0c3abe2d56007e0d11c11c851428504d39d
Andrés <andres@linknovus.com>
2024-09-26 22:43:30 -0500

    Added back sides for cards on landing banner

--- src/routes/overview/home/landing-banner/view.jsx
+++ src/routes/overview/home/landing-banner/view.jsx
@@ -2,15 +2,32 @@ import React from "react";
 
 import "./style.css";
 
-const quantity = 11;
+const card_backs = [
+	"pokemon",
+	"pokemon",
+	"pokemon",
+	"digimon",
+	"one-piece",
+	"pokemon",
+	"one-piece",
+	"digimon",
+	"pokemon",
+	"pokemon",
+	"pokemon",
+];
 
 const LandingBannner = () => {
 	return (
 		<>
-			<div style={{ "--quantity": quantity }}>
-				{Array.from({ length: quantity }).map((_, index) => (
+			<div style={{ "--quantity": card_backs.length }}>
+				{card_backs.map((back, index) => (
 					<div key={index} style={{ "--position": index + 1 }}>
 						<img src={require(`./assets/${index + 1}.png`)} alt={index + 1} />
+						<img
+							src={require(`./assets/${back}.png`)}
+							className="back"
+							alt={index + 1}
+						/>
 					</div>
 				))}
 			</div>
