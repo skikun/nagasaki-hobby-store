cbba3c49edfcce083137209d2b2a4ca417aeedc1
Andrés <andres@linknovus.com>
2024-12-07 21:48:40 -0500

    Added basic logic for paginator component.

---
+++ src/components/paginator/component.jsx
@@ -0,0 +1,13 @@
+import "./style.css";
+
+const Component = ({ total, current }) => {
+	return (
+		<div>
+			{total.map((page) => {
+				page;
+			})}
+		</div>
+	);
+};
+
+export default Component;
