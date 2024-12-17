89f6d2d97d105bf8dc7d6749a57f93d73b974b03
Andrés <andres@linknovus.com>
2024-12-17 14:08:03 -0500

    Fixed small issues regarding events API.

--- src/api/events/api.js
+++ src/api/events/api.js
@@ -2,13 +2,16 @@ import { GET } from "./endpoints";
 import { CONFIG } from "../config";
 
 const EVENTS = {
-	get: async (id) => {
-		const response = await (
-			await fetch(id ? `${GET}/${id}` : GET, {
-				...CONFIG,
-			})
-		).json();
-		return response;
+	get: async () => {
+		const response = await fetch(GET, {
+			...CONFIG,
+		});
+
+		const json = await response.json();
+
+		const events = json.events;
+
+		return { events };
 	},
 };
 
